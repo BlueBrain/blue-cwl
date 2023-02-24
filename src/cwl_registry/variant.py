9cc03a555bc371040f21adc00c8ef7e773563318
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2023-02-24 12:35:54 +0000

    Allow retrieving the registry's  latest version

--- src/cwl_registry/variant.py
+++ src/cwl_registry/variant.py
@@ -1,6 +1,7 @@
 """Variant entry."""
 import importlib.resources
 import logging
+import re
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, Optional
@@ -51,8 +52,16 @@ class Variant:
         return cls(**retrieve_variant_data(forge, resource_id, staging_dir))
 
     @classmethod
-    def from_registry(cls, generator_name: str, variant_name: str, version: str) -> "Variant":
-        """Create a variant intance from the package's registry."""
+    def from_registry(
+        cls, generator_name: str, variant_name: str, version: Optional[str] = None
+    ) -> "Variant":
+        """Create a variant intance from the package's registry.
+
+        Args:
+            generator_name: The name of the generator group. Example: cell_composition
+            variant_name: The name of the variant definition. Example: cell_composition_manipulation
+            version: The variant's version. If None the latest release will be used. Example: v0.3.1
+        """
         return _get_variant(generator_name, variant_name, version)
 
     @property
@@ -101,8 +110,7 @@ def _get_variant(generator_name: str, variant_name: str, version: str):
     package_path = importlib.resources.files("cwl_registry")
     L.debug("Package path: %s", package_path)
 
-    variant_spec = _get_variant_spec(generator_name, variant_name, version)
-    variant_dir = _check_directory_exists(package_path / variant_spec)
+    variant_dir = _get_variant_dir(package_path, generator_name, variant_name, version)
 
     configs = get_directory_contents(variant_dir / CONFIGS_DIR_NAME)
     resources = get_directory_contents(variant_dir / RESOURCES_DIR_NAME)
@@ -111,7 +119,7 @@ def _get_variant(generator_name: str, variant_name: str, version: str):
     return Variant(
         name=variant_name,
         generator_name=generator_name,
-        version=str(version),
+        version=variant_dir.name,
         configs=configs,
         resources=resources,
         definitions=definitions,
@@ -124,5 +132,45 @@ def _check_directory_exists(directory: Path) -> Path:
     return directory
 
 
-def _get_variant_spec(generator_name: str, variant_name: str, version: str) -> str:
-    return f"generators/{generator_name}/{variant_name}/{version}"
+def _get_variant_dir(package_path, generator_name: str, variant_name: str, version: str) -> str:
+    root_dir = _check_directory_exists(package_path / "generators")
+    generator_dir = _check_directory_names(root_dir / generator_name)
+    variant_dir = _check_directory_names(generator_dir / variant_name)
+
+    if version is None:
+        version_dir = _get_latest_release_dir(variant_dir)
+    else:
+        version_dir = _check_directory_names(variant_dir / version)
+
+    return version_dir
+
+
+def _get_latest_release_dir(variant_dir: Path):
+    release_dirs = _sorted_versions(
+        v for v in variant_dir.iterdir() if v.is_dir() and _is_release(v.name)
+    )
+    if not release_dirs:
+        raise CWLRegistryError(f"No versions found in {variant_dir}")
+    return release_dirs[-1]
+
+
+def _sorted_versions(version_paths):
+    def as_int_tuple(version_dir: Path):
+        tup = version_dir.name[1:].split(".")
+        return int(tup[0]), int(tup[1]), int(tup[2])
+
+    return sorted(version_paths, key=as_int_tuple)
+
+
+def _is_release(version_string):
+    """Return True for a release, e.g. v0.3.1 but not v.0.3.1.dev0"""
+    return re.match(r"^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$", version_string) is not None
+
+
+def _check_directory_names(directory: Path):
+    if not directory.is_dir():
+        names = sorted(x.name for x in directory.parent.iterdir() if x.is_dir())
+        raise CWLRegistryError(
+            f"Directory '{directory.name}' does not exist. " f"Available names: {names}"
+        )
+    return directory
--- tests/unit/test_variant.py
+++ tests/unit/test_variant.py
@@ -1,25 +1,27 @@
+import re
+import tempfile
 import pytest
 from pathlib import Path
 from cwl_registry.exceptions import CWLRegistryError
 from cwl_registry import variant as tested
 
 
-VERSION = "v0.3.0"
+_VERSION = "v0.3.1"
 
 
 @pytest.fixture
 def variant():
     return tested.Variant.from_registry(
-        generator_name="me_type_property",
-        variant_name="neurons_me_type_property",
-        version=VERSION,
+        generator_name="testing",
+        variant_name="position",
+        version=_VERSION,
     )
 
 
 def test_variant__attributes(variant):
-    assert variant.name == "neurons_me_type_property"
-    assert variant.generator_name == "me_type_property"
-    assert variant.version == VERSION
+    assert variant.name == "position"
+    assert variant.generator_name == "testing"
+    assert variant.version == _VERSION
 
 
 def test_variant__get_config_file(variant):
@@ -59,11 +61,76 @@ def test_get_variant(variant):
     assert set(resources) == {"variant_config.yml"}
 
 
-def test_get_variant_spec():
-    spec = tested._get_variant_spec("me_type_property", "neurons_me_type_property", "latest")
-    assert spec == "generators/me_type_property/neurons_me_type_property/latest"
+def test_get_variant__latest():
+    variant = tested.Variant.from_registry("testing", "position")
+    assert variant.version == "v0.3.1"
 
 
 def test_check_directory_exists():
     with pytest.raises(CWLRegistryError, match="Directory 'asdf' does not exist."):
         tested._check_directory_exists(Path("asdf"))
+
+
+def test_get_latest_release_dir():
+    with tempfile.TemporaryDirectory() as tdir:
+        Path(tdir, "v0.3.1").mkdir()
+        Path(tdir, "v1.2.0").mkdir()
+        Path(tdir, "v0.0.2").mkdir()
+
+        res = tested._get_latest_release_dir(Path(tdir))
+
+        assert res.name == "v1.2.0"
+
+
+def test_check_directory_names():
+    with tempfile.TemporaryDirectory() as tdir:
+        Path(tdir, "dir1").mkdir()
+        Path(tdir, "dir2").mkdir()
+        Path(tdir, "file1").touch()
+        Path(tdir, "file2").touch()
+
+        expected = "Directory 'dir3' does not exist. Available names: ['dir1', 'dir2']"
+        with pytest.raises(CWLRegistryError, match=re.escape(expected)):
+            tested._check_directory_names(Path(tdir, "dir3"))
+
+
+def test_sorted_versions():
+    versions = [
+        "v0.0.1",
+        "v0.0.10",
+        "v10.0.0",
+        "v0.1.0",
+        "v1.0.0",
+        "v0.10.0",
+        "v0.0.0",
+    ]
+
+    paths = [Path(f"generators/my-gen/my-var/{v}") for v in versions]
+
+    res = [p.name for p in tested._sorted_versions(paths)]
+
+    assert res == [
+        "v0.0.0",
+        "v0.0.1",
+        "v0.0.10",
+        "v0.1.0",
+        "v0.10.0",
+        "v1.0.0",
+        "v10.0.0",
+    ]
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        ("v0", False),
+        ("v0.1", False),
+        ("v0.1.dev0", False),
+        ("v0.3.1", True),
+        ("v0.3.1.dev0", False),
+        ("v0.3.1.2", False),
+        ("v10.0.0v0.1.2", False),
+    ],
+)
+def test_is_release(version, expected):
+    assert tested._is_release(version) is expected
